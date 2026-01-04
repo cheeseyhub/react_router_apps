9c66edad18e4058abb026b9b8886c26fbcde82e6
CheeseyHub <muhammedibrahim613@gmail.com>
2026-01-04 22:18:31 +0500

    understanding react

--- react_from_scratch/app.js
+++ react_from_scratch/app.js
@@ -1,3 +1,11 @@
-console.log("Hi");
+const React = {
+    createElement: (...args) => {
+        console.log(args);
+    },
+};
+const App = (React.createElement("div", { draggable: true },
+    React.createElement("h2", null, "Hello React!"),
+    React.createElement("p", null, "I am a paragraph."),
+    React.createElement("input", { type: "text" })));
 export {};
 //# sourceMappingURL=app.js.map
--- react_from_scratch/app.tsx
+++ react_from_scratch/app.tsx
@@ -1,1 +1,12 @@
-console.log("Hi");
+const React = {
+  createElement: (...args: any[]) => {
+    console.log(args);
+  },
+};
+const App = (
+  <div draggable>
+    <h2>Hello React!</h2>
+    <p>I am a paragraph.</p>
+    <input type="text" />
+  </div>
+);
